a187f362d9c4b58c5bdfec70fb31fe4ac73e4576
avezzu <avezzu@ethz.ch>
2024-01-07 20:02:03 +0100

    added average calculator

---
+++ scripts/calculate.js
@@ -0,0 +1,81 @@
+function roundToDecimalPlaces(number, decimalPlaces) {
+    let factor = Math.pow(10, decimalPlaces);
+    return Math.round(number * factor) / factor;
+}
+
+function calculate() {
+    const rows = document.getElementsByTagName("tr");
+    let grades = [];
+    let credits = [];
+
+    for (const element of rows) {
+        const row = element;
+        const cells = row.getElementsByTagName("td");
+
+
+        if (cells.length < 6) {
+            continue;
+        }
+        if ((cells[0].textContent.includes("Basisprüfungsblock") || cells[0].textContent.includes("Examination Block"))) {
+
+            const grade = parseFloat(cells[2].textContent);
+            const creadits = parseFloat(cells[4].textContent);
+            if (!isNaN(grade) && !isNaN(creadits)) {
+                grades.push(grade);
+                credits.push(creadits);
+            }
+            continue;
+        }
+        const grade = parseFloat(cells[3].textContent);
+        const creadits = parseFloat(cells[5].textContent);
+        if (!isNaN(grade) && !isNaN(creadits)) {
+            grades.push(grade);
+            credits.push(creadits);
+        }
+    }
+
+    let mean = roundToDecimalPlaces(grades.reduce((a, b) => a + b, 0) / grades.length, 2);
+    let weightedMean = 0;
+    let sumCredits = credits.reduce((a, b) => a + b, 0);
+    for (let i = 0; i < grades.length; i++) {
+        weightedMean += grades[i] * (credits[i] / sumCredits);
+    }
+
+    weightedMean = roundToDecimalPlaces(weightedMean, 2);
+
+    if (window.location.href.includes("lang=en")) {
+        alert("Mean: " + mean + "\nWeighted Mean: " + weightedMean);
+    } else {
+        alert("Durchschnitt: " + mean + "\nGewichteter Durchschnitt: " + weightedMean);
+    }
+
+}
+
+const init = function () {
+    const navebar = document.getElementById("metaNavi");
+    const ul = navebar.getElementsByTagName("ul")[0];
+    const element = document.createElement('li');
+    const link = document.createElement('a');
+    if (window.location.href.includes("lang=en")) {
+        link.textContent = "Average";
+    } else {
+        link.textContent = "Durchschnitt";
+    }
+
+    link.addEventListener('click', calculate);
+    link.style.textDecoration = "none";
+    link.addEventListener('mouseover', function () {
+        link.style.textDecoration = "underline";
+    });
+
+    link.addEventListener('mouseout', function () {
+        link.style.textDecoration = "none";
+    });
+
+    link.style.cursor = "pointer";
+    element.appendChild(link);
+    ul.insertBefore(element, ul.getElementsByTagName("li")[1]);
+}
+
+init();
+
